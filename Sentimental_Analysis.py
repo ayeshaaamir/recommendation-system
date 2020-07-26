77a8a3606c7dd6a285cb048bcc7b098c25078216
Muhammad Rafay @eyetech <61602339+Rafay007eyetech@users.noreply.github.com>
2020-07-26 22:14:12 +0500

    Update Sentimental_Analysis.py

--- Sentimental_Analysis.py
+++ Sentimental_Analysis.py
@@ -15,15 +15,39 @@ tmdb.api_key = '98e756301f07f4baba743620fa8ffd63'
 
 from tmdbv3api import Movie
 
-# load the nlp model and tfidf vectorizer from disk
 filename = 'nlp_model.pkl'
 clf = pickle.load(open(filename, 'rb'))
 vectorizer = pickle.load(open('tranform.pkl','rb'))
 def create_sim():
     data = pd.read_csv('main_data.csv')
-    # creating a count matrix
     cv = CountVectorizer()
     count_matrix = cv.fit_transform(data['comb'])
-    # creating a similarity score matrix
     sim = cosine_similarity(count_matrix)
     return data,sim
+def rcmd(m):
+    m = m.lower()
+    try:
+        data.head()
+        sim.shape
+    except:
+        data, sim = create_sim()
+    if m not in data['movie_title'].unique():
+        return('Sorry! The movie your searched is not in our database. Please check the spelling or try with some other movies')
+    else:
+        i = data.loc[data['movie_title']==m].index[0]
+        lst = list(enumerate(sim[i]))
+        lst = sorted(lst, key = lambda x:x[1] ,reverse=True)
+        lst = lst[1:11]
+        l = []
+        for i in range(len(lst)):
+            a = lst[i][0]
+            l.append(data['movie_title'][a])
+        return l
+
+def ListOfGenres(genre_json):
+    if genre_json:
+        genres = []
+        genre_str = ", " 
+        for i in range(0,len(genre_json)):
+            genres.append(genre_json[i]['name'])
+        return genre_str.join(genres)
