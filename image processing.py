568b53e6b268fe58304382bbd606bf427065b2e9
Muhammad Rafay @eyetech <61602339+Rafay007eyetech@users.noreply.github.com>
2020-07-26 21:50:12 +0500

    Update image processing.py

--- image processing.py
+++ image processing.py
@@ -7,3 +7,8 @@ ap.add_argument("-v", "--video ", required=True,
 	help="path to input video file")
 ap.add_argument("-o", "--override", type=int, default=-1,
 	help="whether to force manual frame count")
+args = vars(ap.parse_args())
+
+
+override = False if args["override"] < 0 else True
+total = count_frames(args["video"], override=override)
