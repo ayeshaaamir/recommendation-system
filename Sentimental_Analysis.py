86b5de720074ab5254642e639e57faf70f19736e
Muhammad Rafay @eyetech <61602339+Rafay007eyetech@users.noreply.github.com>
2020-07-26 22:10:49 +0500

    Create Sentimental_Analysis.py

---
+++ Sentimental_Analysis.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, render_template, request
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import json
+import bs4 as bs
+import urllib.request
+import pickle
+import requests
