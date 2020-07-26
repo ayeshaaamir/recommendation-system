ada5dc24ab7a86ee61e04c42496becd5a4e7af0e
Muhammad Rafay @eyetech <61602339+Rafay007eyetech@users.noreply.github.com>
2020-07-26 21:49:39 +0500

    Update image processing.py

--- image processing.py
+++ image processing.py
@@ -2,4 +2,8 @@
 from imutils.video import count_frames
 import argparse
 import os
-
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video ", required=True,
+	help="path to input video file")
+ap.add_argument("-o", "--override", type=int, default=-1,
+	help="whether to force manual frame count")
